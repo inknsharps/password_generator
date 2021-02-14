55473ac4768dd17a56534c8a77cb8f55abac1afd
inknsharps <nchen25@gmail.com>
2021-02-14 14:38:51 -0500

    add prompts for password variables, and exit points for invalid entries

--- script.js
+++ script.js
@@ -28,8 +28,25 @@ var specialArray = specialCharacters.split("");
 function generatePassword() {
   
   // Prompts user for password length, and set passwordLength variable to a number from 8-128
-  var passwordlength = prompt("How long should the password be?\n Please type in a number from 8 to 128.");
+  var passwordLength = prompt("How long should the password be?\n Please type in a number from 8 to 128.");
+    if (passwordLength === null) {
+      alert("If you want to generate a password again, please click the generate password button!");
+    } else if (passwordLength < 8 || passwordLength > 128) {
+      alert("You must type in a number from 8 to 128!");
+      generatePassword();
+    } else {
+    // Password character inclusion variables, sets to true or false
+    var includeLowercase = confirm("Do you want lowercase letters in this password?");
+    var includeUppercase = confirm("Do you want uppercase letters in this password?");
+    var includeNumber = confirm("Do you want numbers in this password?");
+    var includeSpecial = confirm("Do you want special characters in this password?");
+      if (includeLowercase === false && includeUppercase === false && includeNumber === false && includeSpecial === false) {
+        alert("You must select at least one character type to include!");
+        generatePassword();
+      }                           
+    } 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Password Random generator
