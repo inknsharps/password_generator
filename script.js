a841681fe5ad3235896c8c51a6259bdd191c4db4
inknsharps <nchen25@gmail.com>
2021-02-14 17:57:49 -0500

    add password generation loop, cleaned up alert messages

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Arrays for characters
+// Arrays for the separate character types
 // Generates arrays for both uppercase and lowercase letters
 var letters = "abcdefghijklmnopqrstuvwxyz";
 var lowercaseArray = letters.split("");
@@ -25,17 +25,18 @@ var specialArray = specialCharacters.split("");
 
 // Generate a new password functionality
 function generatePassword() {
-  // Reset choices for characters to be generated
+  // Resets the filtered choice array in case the user triggers a failsafe so previously chosen criteria does not carry over
   var filteredArray = [];
   // Prompts user for password length, and set passwordLength variable to a number from 8-128
-  var passwordLength = prompt("How long should the password be?\n Please type in a number from 8 to 128.");
+  var passwordLength = prompt("How long should the password be?\nPlease type in a number from 8 to 128.");
+    // Failsafe if user clicks cancel during the prompt
     if (passwordLength === null) {
       alert("If you want to generate a password again, please click the generate password button!");
+    // Failsafe for user entering a number outside of intended values
     } else if (passwordLength < 8 || passwordLength > 128) {
-      alert("You must type in a number from 8 to 128!");
-      generatePassword();
+      alert("You must type in a number from 8 to 128!\nClick \"Generate Password\" again.");
     } else {
-      // Password character inclusion variables and array edits
+      // Character type selection prompts for creating the filteredArray, that will be used for password generation
       var includeLowercase = confirm("Do you want lowercase letters in this password?");
         if (includeLowercase === true) {
           var step1Array = [...lowercaseArray];
@@ -60,12 +61,19 @@ function generatePassword() {
         } else {
           filteredArray = [...step3Array];
         }
-        // Failsafe in case user selects no character options
+        // Failsafe in case user selects no to all character options
         if (includeLowercase === false && includeUppercase === false && includeNumber === false && includeSpecial === false) {
-          alert("You must select at least one character type to include!");
-          generatePassword();
+          alert("You must select at least one character type to include to generate a password!\nClick \"Generate Password\" again.");
         }
+    }   
+    // Initialize array for randomly generated password
+    var genCharacter = [];
+    // For loop that, 1) Get a random value from the filtered array, 2) Pushes it to add it to the genCharacter array, and repeats until it reaches the user defined password length
+    for (var i = 0; i < passwordLength; i++) {
+      genCharacter.push(filteredArray[Math.floor(Math.random() * filteredArray.length)]);
     }
+    // Joins together the values in the genCharacter array, and returns it to display on the HTML element
+    return genCharacter.join("");
 }
 
 // Add event listener to generate button
