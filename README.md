4a42c689f6552dac0e3e8ff480381b41c2ce1b90
inknsharps <nchen25@gmail.com>
2021-02-17 00:05:16 -0500

    v2 revision upload, move v1 to separate folder, update readme

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ This is a password generator that can generate a password using a selection of l
 * Easily add more characters to the generation pool by editing the character arrays in `script.js`.
 
 ## Usage
+<br>
+<img src="./v1/assets/gen_demo.gif">
+<br>
 
 * Open up index.html.
 * Click the Generate Password button.
@@ -18,19 +21,23 @@ This is a password generator that can generate a password using a selection of l
 
 Link to a deployed version online can be found <a href="https://inknsharps.github.io/password_generator/">here</a>.
 
-## Demos and Screenshots
+## Notable Changes from v1 to v2
 
-### Password Generation
-<img src="./assets/gen_demo.gif">
+v1's Javascript code utilized predefined arrays for the character types and appended them together with the spread/`...` syntax, based on a sequence of `if...else` statements. The downside of this is the need of declaring new arrays each time a character type needed to be added to create the final array in which the password would be generated from. The code used a total of **9 different arrays!**
 
-### Editable Arrays
-<img src="./assets/character_arrays.jpg">
+v2 aims to cut down on the array reliance. It instead utilitizes a `.fromCharCode` method to generate characters randomly based on their UTF-16 character code. Unfortunately, this doesn't work great for symbols, so this is still defined as a string, but instead of converting it to an array, a simple `.charAt` method was applied to randomly pull a value from there. 
+Another result of cutting down on array reliance is instead of generating a long array in which the password is generated from, we use the following sequence:
 
+1. We randomly generate a short array, which contains characters of the types we've chosen
+2. We grab a value from that short array and add it to our final password
+3. Repeat steps one and two until the password length is met.
 
+This way, we only make use of **two arrays** in the entire solution. Pretty cool, huh?
 
 ## Credits
 
 Columbia Engineering BCS for the initial HTML, CSS and JS work.
+Florin Pop's <a href="https://codepen.io/FlorinPop17/pen/BaBePej">Random Password Generator</a> for inspiration on random password generation logic.
 
 ## Disclaimer
 
--- assets/script.js
+++ assets/script.js
@@ -2,81 +2,95 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(){
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-}
-
-// Arrays for the separate character types
-// Generates arrays for both uppercase and lowercase letters
-var letters = "abcdefghijklmnopqrstuvwxyz";
-var lowercaseArray = letters.split("");
-var uppercaseArray = letters.toUpperCase().split("");
-
-// Generates arrays for numbers
-var num = "1234567890";
-var numArray = num.split("");
-
-// Generates arrays for special characters
-var specialCharacters = ("!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"); 
-var specialArray = specialCharacters.split("");
-
-// Generate a new password functionality
-function generatePassword() {
-  // Resets the filtered choice array in case the user triggers a failsafe so previously chosen criteria does not carry over
-  var filteredArray = [];
-  // Prompts user for password length, and set passwordLength variable to a number from 8-128
-  var passwordLength = prompt("How long should the password be?\nPlease type in a number from 8 to 128.");
-    // Failsafe if user clicks cancel during the prompt
-    if (passwordLength === null) {
-      alert("If you want to generate a password again, please click the generate password button!");
-    // Failsafe for user entering a number outside of intended values
-    } else if (passwordLength >= 8 && passwordLength <= 128) {
-      // Character type selection prompts for creating the filteredArray, that will be used for password generation
-      var includeLowercase = confirm("Do you want lowercase letters in this password?");
-        if (includeLowercase === true) {
-          var step1Array = [...lowercaseArray];
-        } else {
-          step1Array = [];
-        }
-      var includeUppercase = confirm("Do you want uppercase letters in this password?");
-        if (includeUppercase === true) {
-          var step2Array = [...step1Array, ...uppercaseArray];
-        } else {
-          step2Array = [...step1Array];
-        }
-      var includeNumber = confirm("Do you want numbers in this password?");
-        if (includeNumber === true) {
-          var step3Array = [...step2Array, ...numArray];
-        } else {
-          step3Array = [...step2Array];
-        }
-      var includeSpecial = confirm("Do you want special characters in this password?");
-        if (includeSpecial === true) {
-          filteredArray = [...step3Array, ...specialArray];
-        } else {
-          filteredArray = [...step3Array];
-        }
-        // Failsafe in case user selects no to all character options
-        if (includeLowercase === false && includeUppercase === false && includeNumber === false && includeSpecial === false) {
-          alert("You must select at least one character type to include to generate a password!\nClick \"Generate Password\" again.");
-          return "Error!";
-        }
-    } else {
-      alert("You must type in a number from 8 to 128!\nClick \"Generate Password\" again.");
-      return "Error!";
-    }  
-    // Initialize array for randomly generated password
-    var genCharacter = [];
-    // For loop that, 1) Get a random value from the filtered array, 2) Pushes it to add it to the genCharacter array, and repeats until it reaches the user defined password length
-    for (var i = 0; i < passwordLength; i++) {
-      genCharacter.push(filteredArray[Math.floor(Math.random() * filteredArray.length)]);
-    }
-    // Joins together the values in the genCharacter array, and returns it to display on the HTML element
-    return genCharacter.join("");
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Character and length checks
+// Define check variables in global scope
+var lowercaseCheck;
+var uppercaseCheck;
+var numberCheck;
+var symbolCheck;
+// Define Length of password value in global scope
+var passwordLength;
+
+// Function to prompt user of which characters to include in password, and set boolean values
+function parameterChecks(){
+  lowercaseCheck = confirm("Do you want lowercase letters in this password?");
+  uppercaseCheck = confirm("Do you want uppercase letters in this password?");
+  numberCheck = confirm("Do you want numbers in this password?");
+  symbolCheck = confirm("Do you want special characters in this password?");
+}
+
+// Random password generation functionality 
+// Define arrays and final password string variables
+var passwordGenArray;
+var finalPasswordArray;
+var finalPassword;
+
+// Function to randomize password character selection array
+function randomizeArray(){
+    // Clears array for generated characters
+    passwordGenArray = [];
+    // Statements to push a randomly generated character of a specific type to passwordGenArray
+    if (lowercaseCheck === true){
+        passwordGenArray.push(getRandomLowerCase());
+    }
+    if (uppercaseCheck === true){
+        passwordGenArray.push(getRandomUpperCase());
+    }
+    if (numberCheck === true){
+        passwordGenArray.push(getRandomNumber());
+    }    
+    if (symbolCheck === true){
+        passwordGenArray.push(getRandomSpecial());
+    }
+    // Functions for generating characters
+    function getRandomLowerCase(){
+        return String.fromCharCode(Math.floor(Math.random()*26)+97);
+    }
+    function getRandomUpperCase(){
+        return String.fromCharCode(Math.floor(Math.random()*26)+65);
+    }
+    function getRandomNumber(){
+        return String.fromCharCode(Math.floor(Math.random()*10)+48);
+    }
+    function getRandomSpecial(){
+        var symbol = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+        return symbol.charAt(Math.floor(Math.random()*symbol.length));
+    }
+}
+
+// Generate random password functionality
+function generatePassword(){
+  // Prompts user to define length of password
+  passwordLength = parseInt(prompt("How long should the password be?\nPlease type in a number from 8 to 128."));
+  // Failsafe if user doesn't enter a valid number
+  if (Number.isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("You must type in a number from 8 to 128!\nClick \"Generate Password\" again.");
+    return "Error!";
+  } else {
+  parameterChecks();
+  // Failsafe if user selects false on all character types
+  if (lowercaseCheck === false && uppercaseCheck === false && numberCheck === false && symbolCheck === false){
+    alert("You must select at least one character type to include to generate a password!\nClick \"Generate Password\" again.");
+    return "Error!";
+  }
+  // Clears finalPasswordArray of any old values
+  finalPasswordArray = [];
+  // For loop that repeats based on passwordLength value
+  for (var i = 0; i < passwordLength; i++){
+      randomizeArray();
+      finalPasswordArray.push(passwordGenArray[Math.floor(Math.random()*passwordGenArray.length)]);
+  }
+  // Join together generated array to a string, and returns it
+  finalPassword = finalPasswordArray.join("");
+  return finalPassword;
+  }
+}
